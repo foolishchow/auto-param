09cfacf8624dd02a8988acc924a568cc1b58e8f9
foolishchow <foolishchow@dingtalk.com>
2021-04-23 17:00:55 +0800

    update

--- multi-module/src/main/groovy/me/foolishchow/android/multimodule/mutilmodule/IMultiModulePluginExtension.groovy
+++
@@ -1,7 +0,0 @@
-package me.foolishchow.android.multimodule.mutilmodule
-
-public interface IMultiModulePluginExtension {
-    void include(String projectName);
-    Set<String> getProjects();
-    void addModuleIncludeListener(OnModuleIncludeListener listener)
-}
--- multi-module/src/main/groovy/me/foolishchow/android/multimodule/mutilmodule/OnModuleIncludeListener.groovy
+++
@@ -1,5 +0,0 @@
-package me.foolishchow.android.multimodule.mutilmodule
-
-interface OnModuleIncludeListener {
-    void addIncludeModule(String projectName)
-}
